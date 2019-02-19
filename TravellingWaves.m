236b129554e8e83a884e6cee7d341d3b5721034e
Erik Forsberg <erikforsberg95@hotmail.com>
2019-02-19 17:05:29 +0100

    started on waves

--- TravellingWaves.m
+++ TravellingWaves.m
@@ -1,3 +1,17 @@
 %Problem 1, Travelling waves
 
-% b) 
+t = 100; 
+x = 100; 
+I = zeros(x,1); 
+S = zeros(x,1); 
+
+heatMap = zeros(x,t); 
+for i = t
+    heatMap(:,i) = S; 
+    nextS = SusceptibleProgression(I,S); 
+    nextI = InfectedProgression(I,S); 
+    
+    
+    
+    
+end
