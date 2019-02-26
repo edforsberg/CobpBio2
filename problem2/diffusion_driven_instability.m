b1e22ca4febe4e027ed55300b2385bdbeeeeef9b
Erik Forsberg <erikforsberg95@hotmail.com>
2019-02-26 11:30:09 +0100

    fuck u gitler

--- problem2/diffusion_driven_instability.m
+++ problem2/diffusion_driven_instability.m
@@ -1,6 +1,24 @@
 L = 128;
 a = 3;
 b = 8;
+Du = 1; 
+Dv = 2.3; %3 5 9
+dt = 0.01; 
 latice = zeros(L,L);
-x0 = a;
-y0 = b/a;
+u = a;
+v = b/a;
+timeSteps = 1000; 
+snapShot =  500; 
+
+
+for i = 1:timeSteps 
+    if(i == snapShot) 
+        snapframe = u; 
+    end
+    nextU = du(u,v,a,b,Du,dt); 
+    nextV = dv(u,v,b,Dv,dt); 
+    u = nextU; 
+    v = nextV;    
+end
+
+surf(snapframe); 
