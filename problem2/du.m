a788125a089008f1f75e4edcf199cf842281289c
Erik Forsberg <erikforsberg95@hotmail.com>
2019-02-26 11:15:49 +0100

    functions

---
+++ problem2/du.m
@@ -0,0 +1,6 @@
+function nextU = du(u,v,a,b,Du,dt)
+uFunc = a-(b-1).*u+u.^2.*v; 
+uDiff = Du*Diffusion(u); 
+uDot = uFunc+uDiff; 
+nextU = u+uDot*dt; 
+end
---
+++ problem2/dv.m
@@ -0,0 +1,6 @@
+function nextV = dv(u,v,b,Dv,dt)
+vFunc = b*u-u.^2*v;
+vDiff = Dv*Diffusion(v); 
+vDot = vFunc+vDiff; 
+nextV = v+vDot*dt; 
+end
