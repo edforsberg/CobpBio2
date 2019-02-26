b712942cada859f5c3f274f463ebcc3961027e9d
Sebastian Nilsson <sebbehebbe@gmail.com>
2019-02-26 11:36:28 +0100

    Diffusion EHRMAGEEEERD

--- problem2/Diffusion.m
+++ problem2/Diffusion.m
@@ -1,3 +1,10 @@
 function res = Diffusion(lattice)
-    res = del2([lattice(1,:); lattice(:,1) lattice lattice(:,end); lattice(end,:)]);
+    periodic_lattice = [lattice(:,end) lattice lattice(:,1)];
+    periodic_lattice = [periodic_lattice(end,:); periodic_lattice; periodic_lattice(1,:)];
+    periodic_lattice(1,1) = 0;
+    periodic_lattice(1,end) = 0;
+    periodic_lattice(end,1) = 0;
+    periodic_lattice(end,end) = 0;
+    periodic_lattice = del2(periodic_lattice);
+    res = periodic_lattice(2:end-1,2:end-1);
 end
