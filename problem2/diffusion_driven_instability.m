dc05065c0d3a23f82a45915559fdc1de4336b62d
Sebastian Nilsson <sebbehebbe@gmail.com>
2019-02-26 15:14:44 +0100

    GRAPHZ

--- problem2/diffusion_driven_instability.m
+++ problem2/diffusion_driven_instability.m
@@ -2,15 +2,16 @@ L = 128;
 a = 3;
 b = 8;
 Du = 1; 
-Dv = 3.4; 
+Dv = 9; 
 dt = 0.01; 
 latice = ones(L,L);
 u = latice.*a + (-0.1 + rand([L L])*0.2)*a;
 v = latice.*(b/a)+(-0.1 + rand([L L])*0.2)*(b/a);
 timeSteps = 1000; 
-snapFrame =  50; 
+snapFrame =  999; 
 
 
+colormap hot
 for i = 1:timeSteps 
     if(i == snapFrame) 
         snapShot = u; 
@@ -19,11 +20,15 @@ for i = 1:timeSteps
     nextV = dv(u,v,b,Dv,dt); 
     u = nextU; 
     v = nextV;    
+
 end
 
-colormap hot
-contourf(snapShot,'edgecolor','none');
+
+
 
 %su(snapShot, 'EdgeColor','none','LineStyle','none','FaceLighting','phong'); 
-
+contourf(snapShot, 'edgecolor', 'none'); 
+caxis([0 10])
+colorbar;
+drawnow;
 colorbar
